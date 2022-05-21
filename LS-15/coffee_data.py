4d21cc3431271098b52ea1e958cc674ce87b5f68
annasugoniako <anna.sugoniako@pandadoc.com>
2022-05-21 14:32:11 +0300

    LS-14, LS-15

---
+++ LS-15/coffee_data.py
@@ -0,0 +1,22 @@
+coffee_menu = [
+    {
+        'coffee_type': 'espresso',
+        'water': 50,
+        'milk': 0,
+        'coffee': 18,
+        'cost': 1.5,
+    },
+    {
+        'coffee_type': 'latte',
+        'water': 200,
+        'milk': 150,
+        'coffee': 24,
+        'cost': 2.5,
+    },
+    {
+        'coffee_type': 'cappuccino',
+        'water': 250,
+        'milk': 100,
+        'coffee': 24,
+        'cost': 3.0,
+    }]
